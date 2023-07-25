91a41584303d953723ca63d0f69fd6f28d57b4df
荣 <15035291867@163.com>
2023-07-25 08:57:08 +0800

    chore: 添加antd design vue库 并配置其自动按需引入

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    // 自动按需引入
+    Components({
+        resolvers: [
+            AntDesignVueResolver({
+                importStyle: false, // css in js
+            })
+        ]
+    })
   ],
   resolve: {
     alias: {
