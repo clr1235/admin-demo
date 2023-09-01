66079f1689f6e85a7f78a73a0ffbca50a5e55819
荣 <15035291867@163.com>
2023-09-01 08:59:09 +0800

    feat: 添加nprogress

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+
+
+router.beforeEach(() => {
+    NProgress.configure({ showSpinner: false });
+    NProgress.start()
+})
+router.afterEach(() => {
+    NProgress.done()
+})
 
 app.use(createPinia())
 app.use(router)
